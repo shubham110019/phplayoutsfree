7c9185f023d4504c5830906c088b23f7673af424
shubham <sralli73@gmail.com>
2020-06-06 06:22:27 -0700

    pagination update

--- pagination.php
+++ pagination.php
@@ -1,29 +1,33 @@
 <?php
-include('database.php');
+include "include/header.php";
 $limit = 5;  
 if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; };  
 $start_from = ($page-1) * $limit;  
   
-$sql = "SELECT * FROM user_data ORDER BY name ASC LIMIT $start_from, $limit";  
+$sql = "SELECT * FROM themeall ORDER BY id ASC LIMIT $start_from, $limit";  
 $rs_result = mysqli_query($conn, $sql);  
 ?>
-<table class="table table-bordered table-striped">  
-<thead>  
-<tr>  
-<th>Name</th>  
-<th>Email</th>  
-</tr>  
-</thead>  
-<tbody>  
+
 <?php  
 while ($row = mysqli_fetch_array($rs_result)) {  
 ?>  
-            <tr>  
-            <td><?php echo $row["name"]; ?></td>  
-            <td><?php echo $row["email"]; ?></td>  
-            </tr>  
+
+			<div class="col-md-4">			
+				<div class="card ">
+					<a href="<?= $base_url ?>templates.php?url=<?=$row["theme_url"] ?>">
+				  <div class="view overlay">
+				    <img class="card-img-top" src="<?= $row['theme_image']?>"
+				      alt="Card image cap">
+				   
+				  </div>
+				  <div class="card-body">
+				    <p class="card-text"><b><?=$row["title"] ?></b></p>	
+				  </div>
+				</a>
+				</div>
+			</div>
+
 <?php  
 };  
 ?>  
-</tbody>  
-</table>    
+   
